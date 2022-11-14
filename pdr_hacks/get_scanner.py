65e2a8496ba1005e863e29505ac51b8880766136
Patrick Burke <pjburke@ncsu.edu>
2022-11-14 17:57:06 -0500

    put user authentication into a function

--- pdr_hacks/get_scanner.py
+++ pdr_hacks/get_scanner.py
@@ -3,15 +3,75 @@
 # https://stackoverflow.com/questions/19732978/how-can-i-get-a-string-from-hid-device-in-python-with-evdev
 
 import evdev
+import RPi.GPIO as GPIO
 from evdev import *
 import time 
 #dev =evdev.InputDevice('/dev/input/by-id/usb-SM_SM-2D_PRODUCT_HID_KBW_APP-000000000-event-kbd')
 dev =evdev.InputDevice('/dev/input/by-id/usb-IDTECH_IDTECH_MiniMag_II_USB-HID_Keyboard_Reader-event-kbd')
 dev.grab()
 
+
 # for event in dev.read_loop():
 #     if event.type == ecodes.EV_KEY:
 #         print(categorize(event))
+# Suppresses warning messages from output.
+GPIO.setwarnings(False)
+	
+	# This tells the library to use the GPIO numbers and not the
+	# header pin numbers. If you want to use the pin numbers in
+	# the code below, use GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
+    
+    #add all used gpio pins for configuration
+channel_list = [2,3,4,17,27,14]
+    # Sets up GPIO 2 (Pin 3 as an output) 
+GPIO.setup(channel_list, GPIO.OUT)
+    
+    # Define constants and variables
+
+USER_1=200338060
+USER_2=200248706
+   
+user_1_state=0
+user_2_state=0
+    
+    # Set power pin to on
+GPIO.output(27,True)
+    # Set switch pin to default
+GPIO.output(3,True)
+
+    # Give scanner time to get online
+print("INITIALIZED")
+
+def user_authentication(card):
+        GPIO.output(2,True)
+        print("Woo a card "+x)
+        #check user 1
+        if (( user_1_state == 0 ) and (card== USER_1 )):
+            GPIO.output(4,True)
+            user_1_state=1
+            print("USER 1")
+		#check user 2
+        if (( user_2_state == 0 ) and (card== USER_2 )):
+            GPIO.output(17,True)
+            user_2_state=1
+            print("USER 2")
+            
+        if(user_1_state):
+            GPIO.output(2,True)
+            GPIO.output(3,False)
+            print("ACTIVATED")
+            GPIO.output(14,True)
+        if((card != USER_1) and (card != USER_2)):
+            GPIO.output(2,False)
+            GPIO.output(3,True)
+            print("DISABLED")
+            GPIO.output(14,False)
+            user_1_state =0
+            user_2_state =0
+            
+
+
 
 scancodes = { 
     # Scancode: ASCIICode 
@@ -55,6 +115,7 @@ for event in dev.read_loop():
       if (data.scancode != 42) and (data.scancode != 28): 
        x += key_lookup 
       if(data.scancode == 28): 
-       #print (x)   # Print it all out! 
+       print (x)   # Print it all out! 
+       user_authentication(x)
        x = ''
 
