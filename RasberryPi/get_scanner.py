c871f57674de94808baf0c21fe17c45aa016ef74
Patrick Burke <pjburke@ncsu.edu>
2022-11-28 11:08:13 -0500

    move code into a while loop for updating screen

--- RasberryPi/get_scanner.py
+++ RasberryPi/get_scanner.py
@@ -127,6 +127,7 @@ def user_authentication(card):
                 print("ACTIVATED")
                 GPIO.output(14,True)                # Device enable light
                 endTime = time.time()+countDownIncrementer
+                print("endTime: " + str(endTime))
 
 
     # Test code for keyboard input instead of the keycard reader
@@ -160,30 +161,29 @@ capscodes = {
 x = '' 
 caps = False 
 
-
+while(1):
+ countdown()
     # Loop for the Keycard Reader:
-for event in dev.read_loop(): 
+ for event in dev.read_loop(): 
     
-    if event.type == ecodes.EV_KEY: 
-     data = categorize(event) # Save the event temporarily to introspect it 
-     if data.scancode == 42: 
-      if data.keystate == 1: 
-       caps = True 
-      if data.keystate == 0: 
-       caps = False 
-     if data.keystate == 1: # Down events only 
-      if caps: 
-       key_lookup = u'{}'.format(capscodes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(data.scancode) # Lookup or return UNKNOWN:XX 
-      else: 
-       key_lookup = u'{}'.format(scancodes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(data.scancode) # Lookup or return UNKNOWN:XX 
-      if (data.scancode != 42) and (data.scancode != 28): 
-       x += key_lookup 
-      if(data.scancode == 28): 
-       print (x)   # Print it all out! 
-       user_authentication(x)
-       if(endTime):
-        win.after(1000,)
-        print("windowUpdate")
-        countdown()
-       x = ''
+     if event.type == ecodes.EV_KEY: 
+      data = categorize(event) # Save the event temporarily to introspect it 
+      if data.scancode == 42: 
+       if data.keystate == 1: 
+        caps = True 
+       if data.keystate == 0: 
+        caps = False 
+      if data.keystate == 1: # Down events only 
+       if caps: 
+        key_lookup = u'{}'.format(capscodes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(data.scancode) # Lookup or return UNKNOWN:XX 
+       else: 
+        key_lookup = u'{}'.format(scancodes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(data.scancode) # Lookup or return UNKNOWN:XX 
+       if (data.scancode != 42) and (data.scancode != 28): 
+        x += key_lookup 
+       if(data.scancode == 28): 
+        print (x)   # Print it all out! 
+        user_authentication(x)
+       
+       
+     x = ''
 
