3127119cc8905fdf8ff2e860064a9509ff33e54a
Holly Grant <hmgrant@ncsu.edu>
2023-02-02 12:39:52 -0500

    Demo GUI

--- RasberryPi/gui.py
+++ RasberryPi/gui.py
@@ -28,7 +28,6 @@ def countdown():
 	currentTime =endTime-time.time()
 	#print(int(currentTime/60),":", int(currentTime%60))
 	countDown.config(text=str(int(currentTime/60)) +":" +str(int(currentTime%60)) +" minutes remaining")
-	#countDown.config(text=str(int(currentTime))+":")
 	#print(str(time.localtime().tm_hour) +":"+str(time.localtime().tm_min))
 	
 	
@@ -53,10 +52,11 @@ Grid.columnconfigure(win,0,weight=1)
  
 win.title("Access Control")#window name
 win.geometry('800x480')#size of window
+win.configure(bg='white')
 countDownText = "count"
-countDown = Label(win,text= countDownText ,anchor=CENTER,font= myFont, bg="white") #create label for countdown
+countDown = Label(win,text= countDownText ,anchor=CENTER,font= myFont, bg='white') #create label for countdown
 #countDown.pack()
-countDown.grid(row=2,column=0, sticky="nsew")#puts the countdown to the center of the screen
+#countDown.grid(row=2,column=0, sticky="nsew")#puts the countdown to the center of the screen
 exitButton  = Button(win, text = "Exit", font = myFont, command = exitProgram, height =2 , width = 6) 
 #exitButton.pack(side = BOTTOM)
 
@@ -65,29 +65,90 @@ top= Label(text="ECE Makerspace",anchor=E,font=myFont, fg="white", bg="red")
 top.grid(row=0,column=0)
 
 #User Name Label
-welcome= Label(text="Welcome USER!", anchor=CENTER, font=myFont, bg="white")
-welcome.grid(row=1,column=0)
+#welcome= Label(text="Welcome USER!", anchor=CENTER, font=myFont, bg='white')
+#welcome.grid(row=1,column=0)
 
 #End Session Label
-button=Label(text="Push Button To End Session", anchor=CENTER, font=myFont, bg="white")
-button.grid(row=3,column=0)
+#button=Label(text="Push Button To End Session", anchor=CENTER, font=myFont, bg='white', fg='blue')
+
 
 #Buddy Label
+buddy=Label(text="Buddy Required, Swipe Another ID", anchor=CENTER, font=myFont, bg='white')
 
 #Reswipe Label
+reswipe=Label(text="Reswipe To Continue Session", anchor=CENTER, font=myFont, bg='white', fg='red')
 
 #Authorized Label
+#authorized=Label(text="AUTHORIZED", anchor=CENTER, font=myFont, bg='white', fg='green')
+#not_authorized= Label(text="NOT AUTHORIZED", anchor=CENTER, font=myFont, bg='white', fg='red')
+
+#User Name Label
+#welcome= Label(text="Welcome USER!", anchor=CENTER, font=myFont, bg='white')
 
 #Start Label
+#start=Label(text="Swipe Card To Begin Session", anchor=CENTER, bg='white', font=myFont, fg='blue')
 
+
+
+def createauthorize():
+	authorized=Label(text="AUTHORIZED", anchor=CENTER, font=myFont, bg='white', fg='green')
+	authorized.grid(row=1,column=0)
+
+
+def createnauthorize():
+	not_authorized= Label(text="NOT AUTHORIZED", anchor=CENTER, font=myFont, bg='white', fg='red')
+	not_authorized.grid(row=1,column=0)
+
+
+def createwelcome():
+	welcome= Label(text="Welcome USER!", anchor=CENTER, font=myFont, bg='white')
+	welcome.grid(row=1,column=0)
+
+def createstart():
+	start=Label(text="Swipe Card To Begin Session", anchor=CENTER, bg='white', font=myFont, fg='blue')
+	start.grid(row=1,column=0)
+
+def destroystart():
+	start.destroy()
+
+def createbutton():
+	button=Label(text="Push Button To End Session", anchor=CENTER, font=myFont, bg='white', fg='blue')
+	button.grid(row=3,column=0)
+
+def createcountdown():
+	countDown.grid(row=2,column=0, sticky="nsew")
+	countDown = Label(win,text= countDownText ,anchor=CENTER,font= myFont, bg='white') #create label for countdown
+
+
+newstart=True 
+count=0
+def stepone():
+	authorized=Label(text="AUTHORIZED", anchor=CENTER, font=myFont, bg='white', fg='green')
+	authorized.grid(row=1,column=0)
+	win.after(7000,authorized.destroy)
+
+def steptwo():
+	welcome= Label(text="Welcome USER!", anchor=CENTER, font=myFont, bg='white')		
+	welcome.grid(row=1,column=0)
+	#countDown = Label(win,text= countDownText ,anchor=CENTER,font= myFont, bg='white')
+	countDown.grid(row=2,column=0, sticky="nsew")
+	button=Label(text="Push Button To End Session", anchor=CENTER, font=myFont, bg='white', fg='blue')
+	button.grid(row=3,column=0)
+	newstart= False
 
 endTime = time.time()+countDownIncrementer
 
-
- 
+#main
 while True:
-    if time.time() <= endTime:
-      countdown()
+	if newstart == True:
+		start=Label(text="Swipe Card To Begin Session", anchor=CENTER, bg='white', font=myFont, fg='blue')
+		start.grid(row=1,column=0)
+		win.after(3000, start.config(text="AUTHORIZED", fg='green'))
+		
+		
+	if time.time() <= endTime:
+		countdown()
+	
 
 
 
