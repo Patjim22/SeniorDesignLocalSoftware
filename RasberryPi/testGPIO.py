6bc32f911154a273f1ea785e63be2c42c469d2be
Patrick Burke <pjburke@ncsu.edu>
2023-02-27 20:42:30 -0500

    added gpio pin for usbenable

--- RasberryPi/testGPIO.py
+++ RasberryPi/testGPIO.py
@@ -3,4 +3,4 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)                                   #Use Board pin numbers
 GPIO.setwarnings(False)
 GPIO.setup(37, GPIO.OUT, initial =GPIO.LOW)
-GPIO.output(37,False) 
+GPIO.output(37,True) 
