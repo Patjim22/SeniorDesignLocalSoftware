d3dace4508de861f7eb0d989626e2ab1090a6001
Patrick Burke <pjburke@ncsu.edu>
2023-02-27 20:07:17 -0500

    test program for GPIO

---
+++ RasberryPi/testGPIO.py
@@ -0,0 +1,6 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)                                   #Use Board pin numbers
+GPIO.setwarnings(False)
+GPIO.setup(37, GPIO.OUT, initial =GPIO.LOW)
+GPIO.output(37,False) 
