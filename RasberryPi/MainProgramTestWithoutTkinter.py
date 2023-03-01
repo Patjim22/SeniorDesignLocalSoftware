bb03e4565b1ea0bd592aaa50bdec6878d7e6f01c
pjburke <pjburke@ncsu.edu>
2023-02-28 22:39:30 -0500

    created a testprogram without tkinter

---
+++ RasberryPi/MainProgramTestWithoutTkinter.py
@@ -0,0 +1,240 @@
+import threading
+import time
+import sys
+import re
+#from tkinter import *
+#from tkinter import font
+import RPi.GPIO as GPIO
+
+card = "0"
+global start 
+global endTime 
+endTime=0
+global countDownText
+global user_1_state, user_2_state
+twoSwipeTime = 20        #used to hold how long to wait for a buddy to swipe
+buddySwipeReuiredBy=0   #holds the time to cancel out and say you were rejected because no buddy
+userName =""
+countDownMinutes=1
+endOfWorkingHours=17	#5pm
+beginningOfWorkHours=8	#8am
+user_1_state =0
+user_2_state = 0
+user_1_ID = 0
+user_2_ID = 0
+
+
+
+
+    
+# Add all used gpio pins for configuration
+DEVICEON =21                                     #22 is green LED device On
+USER1LED =23                                    #27 user 1 #17 user 2 yellow LEDs
+USER2LED=33
+DEVICEENABLED =35                                     #4 and 3  RED LEDS
+REDLED2 =37
+CONTROLOPTO =11                                 #2 control opto
+USBSEL =8                                      #14 usb sel
+USBENABLE = 10                                  #when high disables usb ports
+BUZZER = 12
+BUTTON1 = 29
+BUTTON2 = 31
+
+BackUp_USER= {"200248706", "200289830"}
+channel_list = (5,7,21,23,33,35,37,11,8,10,12,29,31)              
+
+
+
+class ID_Check_Thread (threading.Thread):
+    
+    def __init__(self, thread_name, thread_ID):
+        threading.Thread.__init__(self)
+        self.thread_name = thread_name
+        self.thread_ID = thread_ID
+    # helper function to execute the threads
+    def run(self):
+        global card
+        while True:
+            if(card!='0'):
+                print("Reading user card")
+                if(endTime==0):                         #if endtime ==0 then no one is currently using the machine
+                    assignUserToMachine(card)
+                else:
+                    if((user_1_ID ==card) or (user_2_ID ==card)):
+                        assignUserToMachine(card)
+                card ="0"
+                
+                
+            time.sleep(2)   #sleep 2 seconds
+
+class Read_Card_Tread (threading.Thread): #reads the card
+    def __init__(self, thread_name, thread_ID):
+        threading.Thread.__init__(self)
+        self.thread_name = thread_name
+        self.thread_ID = thread_ID
+
+    # helper function to execute the threads
+    def run(self):
+        global card
+        for line in sys.stdin:
+            if  'q' == line.rstrip():
+                break
+            regSearch =re.compile('\+.*')
+            cardNumber = regSearch.match(line)
+            #print(cardNumber)
+            if(cardNumber!=None):
+                card = cardNumber.string[1:10].rstrip()
+                print(cardNumber.group()[1:10].rstrip())
+                print()
+            else:
+                print(len(line))
+                if(len(line)==10):
+                    card = line[0:9]
+                    print(line[0:9])
+            sys.stdin.flush
+            time.sleep(4)
+            
+def setupGPIO():
+    GPIO.setmode(GPIO.BOARD)                                   #Use Board pin numbers
+    GPIO.setwarnings(False)                                    # Suppresses warning messages from output.
+    # Sets all GPIO pins in the chanel list as an output
+    GPIO.setup(channel_list, GPIO.OUT, initial =GPIO.LOW)
+    GPIO.setup(BUTTON1, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sets the reset to a input with a pull up resistor
+    GPIO.setup(BUTTON2, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sets the reset to a input with a pull up resistor
+    GPIO.output(DEVICEON,True)                                # Set power pin to on
+    # Set switch pin to defaults
+    GPIO.output(USBSEL,False)                                  # USB
+    GPIO.output(USBENABLE,False)
+    GPIO.output(CONTROLOPTO,False)                             # Opto-Isolator
+    print("GPIO SETUP")
+
+def countdown(): #does the countdown when it is required
+    global endTime
+    currentTime =endTime-time.time()                            #measures the endtime vs current time
+    #currentTime = time.time()
+    if(currentTime >0):
+        countDown.config(text=str(int(currentTime/60)) +":" +str(int(currentTime%60)))
+    else:
+        endTime =0
+        disableDevice()
+	
+def configurePi():#pull config data from SQL database
+    countDownMinutes # should be editable to change the length of the countdown
+    endOfWorkingHours # changes the end time of the makerspace working hours
+    beginningOfWorkHours # changes the start time of the makerspace working hours
+    twoSwipeTime #deault is 10sec change to give buddy more or less time to swipe after first swipe
+    return
+
+def enableDevice(): #enables the usb and Control OPTO issolators and starts the countdown
+    global endTime
+    GPIO.output(CONTROLOPTO,True)              # Opto
+    GPIO.output(USBSEL,True)                 	# USB
+    GPIO.output(USBENABLE, False)
+    print("ACTIVATED")
+    GPIO.output(DEVICEENABLED,True)                	# Device enable light
+    endTime = time.time()+countDownIncrementer
+
+def disableDevice():
+    global user_1_state , user_2_state,user_2_ID, user_1_ID, endTime, userName
+    GPIO.output(CONTROLOPTO,False)             # Opto
+    GPIO.output(USBSEL,False)                	# USB
+    GPIO.output(USBENABLE, True)
+    GPIO.output(USER2LED,False)               	# User 2 led
+    GPIO.output(USER1LED,False)                # User1 led
+    print("DISABLED")
+    GPIO.output(DEVICEENABLED,False)               	# Device enable light
+    user_1_state =0
+    user_2_state =0
+    user_1_ID =0
+    user_2_ID =0
+    userName =""
+    
+    endTime=0
+
+def pauseDevice():#disables optoControl
+    GPIO.output(CONTROLOPTO,False)             # Opto
+    print("Paused device")
+
+def check_if_authorized(card):
+    global userName, user_1_state, user_2_state
+    USERS ={"100019744","100019747"} #visitor 1 id #visitor 4 id
+    #write user compatison code for sql in this
+    userName
+    if card in BackUp_USER:
+        userName = "Admin"
+        user_1_state=1
+        user_2_state=1
+        return True
+    if card in USERS:
+        return True
+    return False	# function returns true if authorized user otherwise false
+
+def assignUserToMachine(card):
+    global user_1_state , user_2_state, user_1_ID, user_2_ID, buddySwipeReuiredBy
+    authorized = check_if_authorized(card)
+    if(authorized):
+        if(user_1_state==0):
+            user_1_state=1
+            user_1_ID= card
+        if(user_1_state==1):
+            if(card != user_1_ID):
+                user_2_state=1
+                user_2_ID = card
+                buddySwipeReuiredBy=0
+        if(time.localtime().tm_hour<endOfWorkingHours and time.localtime().tm_hour >=beginningOfWorkHours):#during working houres only 1 user is needed
+            if(user_1_state or user_2_state):
+                enableDevice()     
+        else:#outside of normal hours a buddy is required
+            if(user_1_state and user_2_state):
+                enableDevice()
+            else:
+                print("A buddy is required")        #needs to write to a label on the gui
+                buddySwipeReuiredBy=time.time()+twoSwipeTime     
+    else:
+        print("non-authorized user")
+
+def noBuddySwipe():#send to database that id 1 didn't have a buddy
+    user_1_ID
+
+setupGPIO()    
+T1 = True
+T2 = True
+
+
+   
+    
+configurePi()
+
+disableDevice()
+
+countDownIncrementer = countDownMinutes*60 #number of minutes wanted goes where the 1 is
+th1=  ID_Check_Thread("T1",1000)
+th2 = Read_Card_Tread("T2",2000)
+th1.start()
+th2.start()
+
+while T1:
+    if(endTime!=0):
+        countdown()
+    else:
+        print(time.strftime("%I:%M:%S")) #displays time in 12 hour format
+    
+    if(buddySwipeReuiredBy!=0):
+        currentTime =buddySwipeReuiredBy-time.time() 
+        if(currentTime >0):
+            #print you have blank time to swipe
+            print("Time for Buddy Swipe: "+str(int(currentTime/60)) +":" +str(int(currentTime%60)))
+        else:
+            buddySwipeReuiredBy=0
+            noBuddySwipe()
+            
+    if(userName != ""):
+        print("Welcome: "+ userName)
+    else:
+        print("Welcome USER!")
+    
+    
+    time.sleep(.5)  #sleeps for 1/2 a second 
+
+
+print("Exit")
