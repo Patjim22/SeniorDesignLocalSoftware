c6dd94451f73a7fa5067beb3af7b69699c40c1b9
Patrick Burke <pjburke@ncsu.edu>
2023-03-08 11:29:12 -0500

    fixed it to be LOW

--- RasberryPi/testGPIO.py
+++ RasberryPi/testGPIO.py
@@ -9,9 +9,9 @@ GPIO.setup(BUTTON1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BUTTON2,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 while(True):
-    if(GPIO.input(BUTTON1)==GPIO.low):
+    if(GPIO.input(BUTTON1)==GPIO.LOW):
         print("Button 1")
         
-    if(GPIO.input(BUTTON2)==GPIO.low):
+    if(GPIO.input(BUTTON2)==GPIO.LOW):
         print("Button 2")
         
